c6e9e69666fb279b1481ba5f1f91494cc53c6f8c
gperez13 <giovanni_perez4@yahoo.com>
2018-04-02 21:36:43 -0500

    Edited About component

--- src/About/About.js
+++ src/About/About.js
@@ -13,31 +13,28 @@ class AboutComponent extends Component{
 
 		return(
 			<div>
-				<div id="about">
+				<div class="container">
+					<div class="col-lg-12 col-offset-6 centered" id="about">
 
 
-					<h1>Here is an About page!!!</h1>
-					<hr/>
-					<h7>RxChain is a Blockchain based Pharmaceutical 
-					application aimed at bridging the gap between 
-					hospital EMR systems and Pharmacy records while 
-					reducing drug abuse & operation inefficiencies. 
-					It uses IBM’s Hyperledger technology to eliminate 
-					PHI breaches and is displayed through a React front 
-					d interphase. I am currently working on deploying 
-					the Hyperledger model using Docker. </h7>
+						<h1 id="about-title">About RxChain</h1>
+						<br/><br/>
+						<h7>RxChain is a Blockchain based Pharmaceutical 
+						application aimed at bridging the gap between 
+						hospital EMR systems and Pharmacy records while 
+						reducing drug abuse & operation inefficiencies. 
+						It uses IBM’s Hyperledger technology to eliminate 
+						PHI breaches and is displayed through a React front 
+						d interphase. I am currently working on deploying 
+						the Hyperledger model using Docker. </h7>
 
 
-					
+						
+					</div>
+
+
 				</div>
-
 				<Footer />
-
-
-
-
-
-
 			</div>
 
 			)
